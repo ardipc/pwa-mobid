4cec087957748b3326a7bd8b38921e3d4480e407
ahmad ardiansyah <ahmad.ardiansyah@carsworld.co.id>
2021-04-12 14:04:41 +0700

    set to 2s and comment secure

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
       }}
     >
       <Component {...pageProps} />
-      <ToastContainer position="top-center" closeButton={false} autoClose={1500} hideProgressBar={true} />
+      <ToastContainer position="top-center" closeButton={false} autoClose={2000} hideProgressBar={true} />
     </SWRConfig>
   )
 }
